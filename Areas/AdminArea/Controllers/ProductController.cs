207cf9aefd58240ad139f47856d282ec09fe0ac5
RasulRustamli <rasular@code.edu.az>
2022-01-27 06:42:39 +0400

    Add project files.

---
+++ Areas/AdminArea/Controllers/ProductController.cs
@@ -0,0 +1,197 @@
+﻿using FrontToBack.DAL;
+using FrontToBack.Extensions;
+using FrontToBack.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.Areas.AdminArea.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Area("AdminArea")]
+    public class ProductController : Controller
+    {
+        private readonly Context _context;
+        private readonly IWebHostEnvironment _env;
+        public ProductController(Context context,IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            List<Product> products = _context.Products.Include(c => c.Category).ToList();
+            return View(products);
+        }
+
+        public async Task<IActionResult> Detail(int? Id)
+        {
+            if (Id == null) return NotFound();
+            Product dbProduct = await _context.Products.FindAsync(Id);
+            if (dbProduct == null) return NotFound();
+
+            return View(dbProduct);
+        }
+
+        public IActionResult Create()
+        {
+            List<SelectListItem> categories = (from i in _context.Categories.ToList()
+                                               select new SelectListItem
+                                               {
+                                                   Text = i.Name,
+                                                   Value = i.Id.ToString()
+                                               }).ToList();
+            ViewBag.dgr = categories;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            bool isExist = _context.Products.Any(p => p.Name.ToLower().Trim() == product.Name.ToLower().Trim());
+            if (isExist)
+            {
+                ModelState.AddModelError("Name", "The product with this name already exists");
+                View();
+            }
+            
+                if (ModelState["Photo"].ValidationState == Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid)
+                {
+                    ModelState.AddModelError("Photo", "Do not empty");
+                }
+
+                if (!product.Photo.IsImage())
+                {
+                    ModelState.AddModelError("Photo", "only image");
+                    return View();
+                }
+                if (product.Photo.IsCorrectSize(300))
+                {
+                    ModelState.AddModelError("Photo", "300den yuxari ola bilmez");
+                    return View();
+                }
+
+                
+
+                string fileName = await product.Photo.SaveImageAsync(_env.WebRootPath, "img");
+                
+                Product newProduct = new Product
+            {
+                Category = product.Category,
+                Name = product.Name,
+                Price = product.Price,
+                ImageUrl=fileName,
+                CategoryId = product.CategoryId
+
+            };
+            await _context.Products.AddAsync(newProduct);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+            Product dbProduct = await _context.Products.FindAsync(id);
+            if (dbProduct == null) return NotFound();
+            return View(dbProduct);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteProduct(int? id)
+        {
+            if (id == null) return NotFound();
+            Product dbProduct = await _context.Products.FindAsync(id);
+            if (dbProduct == null) return NotFound();
+
+            string path = Path.Combine(_env.WebRootPath, "img", dbProduct.ImageUrl);
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+            _context.Products.Remove(dbProduct);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null) return NotFound();
+            Product dbProduct = await _context.Products.FindAsync(id);
+            if (dbProduct == null) return NotFound();
+            List<SelectListItem> categories = (from i in _context.Categories.ToList()
+                                               select new SelectListItem
+                                               {
+                                                   Text = i.Name,
+                                                   Value = i.Id.ToString()
+                                               }).ToList();
+            ViewBag.dgr = categories;
+            return View(dbProduct);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, Product product)
+        {
+            if (id == null) return NotFound();
+
+            if (!ModelState.IsValid) return View();
+            bool isExist = _context.Products.Any(c => c.Name.ToLower() == product.Name.ToLower().Trim());
+
+            Product isExistProduct = _context.Products.FirstOrDefault(p => p.Id == product.Id);
+
+            if (isExist && !(isExistProduct.Name.ToLower() == product.Name.ToLower().Trim()))
+            {
+                ModelState.AddModelError("Name", "Bu adla product var");
+                return View();
+            };
+
+
+
+            if (product.Photo != null)
+            {
+                if (ModelState["Photo"].ValidationState == Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid)
+                {
+                    ModelState.AddModelError("Photo", "Do not empty");
+                }
+
+                if (!product.Photo.IsImage())
+                {
+                    ModelState.AddModelError("Photo", "only image");
+                    return View();
+                }
+                if (product.Photo.IsCorrectSize(300))
+                {
+                    ModelState.AddModelError("Photo", "300den yuxari ola bilmez");
+                    return View();
+                }
+                Product dbproduct = await _context.Products.FindAsync(id);
+                string path = Path.Combine(_env.WebRootPath, "img", dbproduct.ImageUrl);
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                }
+                string fileName = await product.Photo.SaveImageAsync(_env.WebRootPath, "img");
+
+                dbproduct.Category = product.Category;
+                dbproduct.Name = product.Name;
+                dbproduct.Price = product.Price;
+                dbproduct.ImageUrl = fileName;
+                dbproduct.CategoryId = product.CategoryId;
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ Areas/AdminArea/Controllers/UserController.cs
@@ -0,0 +1,101 @@
+﻿
+using FrontToBack.DAL;
+using FrontToBack.Models;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Area("AdminArea")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly Context _context;
+
+        public UserController(
+            UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,
+            RoleManager<IdentityRole> roleManager,
+            Context context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            var users = _userManager.Users;
+
+            return View(users);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Create(RegisterVM register)
+        {
+            if (!ModelState.IsValid) return View();
+
+
+            AppUser user = new AppUser()
+            {
+                FullName = register.FullName,
+                UserName = register.UserName,
+                Email = register.Email,
+            };
+
+            IdentityResult identityResult = await _userManager.CreateAsync(user, register.Password);
+
+            if (!identityResult.Succeeded)
+            {
+                foreach (var error in identityResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View();
+            }
+            await _signInManager.SignInAsync(user, true);
+
+            return RedirectToAction("Index", "User", new { area = "AdminArea" });
+        }
+
+        public async Task<IActionResult> IsActive(string id)
+        {
+            AppUser user = await _userManager.FindByIdAsync(id);
+
+          
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            if (user.IsActive)
+            {
+                user.IsActive = false;
+            }
+            else
+            {
+                user.IsActive = true;
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
